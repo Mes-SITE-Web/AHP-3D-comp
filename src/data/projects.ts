7acc99b0581aaeee414b7ded20fe10554fd0df7e
DjoAHP <djodjo.vely@gmail.com>
2024-12-21 21:59:06 +0100

    images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export const projects: Project3D[] = [
     id: '1',
     title: 'Pika Toilet Bar',
     description: 'Une composition original mettant en scene "Pikachu est posey" ',
-    thumbnail: './images/pika-toilet-01.jpg',
+    thumbnail: '../images/pika-toilet-01.jpg',
     modelUrl: '/models/still-life.glb',
     tools: ['Blender', 'Cycles', 'Substance Designer'],
     category: 'Pokemon'
@@ -14,25 +14,25 @@ export const projects: Project3D[] = [
     id: '2',
     title: 'Pika Toilet Bar',
     description: 'Une composition original mettant en scene "Pikachu est posey" ',
-    thumbnail: './images/pika-toilet-01.jpg',
+    thumbnail: '../images/pika-toilet-01.jpg',
     modelUrl: '/models/still-life.glb',
     tools: ['Blender', 'Cycles', 'Substance Designer'],
     category: 'Pokemon'
   },
   {
-    id: '2',
+    id: '3',
     title: 'Pika Toilet Bar',
     description: 'Une composition original mettant en scene "Pikachu est posey" ',
-    thumbnail: './images/pika-toilet-01.jpg',
+    thumbnail: '../images/pika-toilet-01.jpg',
     modelUrl: '/models/still-life.glb',
     tools: ['Blender', 'Cycles', 'Substance Designer'],
     category: 'Pokemon'
   },
   {
-    id: '2',
+    id: '4',
     title: 'Pika Toilet Bar',
     description: 'Une composition original mettant en scene "Pikachu est posey" ',
-    thumbnail: './images/pika-toilet-01.jpg',
+    thumbnail: '../images/pika-toilet-01.jpg',
     modelUrl: '/models/still-life.glb',
     tools: ['Blender', 'Cycles', 'Substance Designer'],
     category: 'Pokemon'
