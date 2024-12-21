72a8dc0e33fae12fdfb5f3bdf8c22f5fa79ca4f3
DjoAHP <djodjo.vely@gmail.com>
2024-12-21 22:07:05 +0100

    chemin

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -5,7 +5,7 @@ export const projects: Project3D[] = [
     id: '1',
     title: 'Pika Toilet Bar',
     description: 'Une composition original mettant en scene "Pikachu est posey" ',
-    thumbnail: '../images/pika-toilet-01.jpg',
+    thumbnail: './images/pika-toilet-01.jpg',
     modelUrl: '/models/still-life.glb',
     tools: ['Blender', 'Cycles', 'Substance Designer'],
     category: 'Pokemon'
@@ -14,7 +14,7 @@ export const projects: Project3D[] = [
     id: '2',
     title: 'Pika Toilet Bar',
     description: 'Une composition original mettant en scene "Pikachu est posey" ',
-    thumbnail: '../images/pika-toilet-01.jpg',
+    thumbnail: './images/pika-toilet-01.jpg',
     modelUrl: '/models/still-life.glb',
     tools: ['Blender', 'Cycles', 'Substance Designer'],
     category: 'Pokemon'
@@ -23,7 +23,7 @@ export const projects: Project3D[] = [
     id: '3',
     title: 'Pika Toilet Bar',
     description: 'Une composition original mettant en scene "Pikachu est posey" ',
-    thumbnail: '../images/pika-toilet-01.jpg',
+    thumbnail: './images/pika-toilet-01.jpg',
     modelUrl: '/models/still-life.glb',
     tools: ['Blender', 'Cycles', 'Substance Designer'],
     category: 'Pokemon'
@@ -32,7 +32,7 @@ export const projects: Project3D[] = [
     id: '4',
     title: 'Pika Toilet Bar',
     description: 'Une composition original mettant en scene "Pikachu est posey" ',
-    thumbnail: '../images/pika-toilet-01.jpg',
+    thumbnail: './images/pika-toilet-01.jpg',
     modelUrl: '/models/still-life.glb',
     tools: ['Blender', 'Cycles', 'Substance Designer'],
     category: 'Pokemon'
